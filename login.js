9e926d92a2ac7e5ad8ca603b9e94d8f696b6f476
jeduardovr <79815269+jeduardovr@users.noreply.github.com>
2021-09-13 11:13:15 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,20 +1,21 @@
-"credentials": [
-    {
-      "username": "jvelazquez",
-      "password": "123456",
-      "information": [
-          "name": "Eduardo Velazquez",
-          "age": 27
-          "email": "eduardovelazquez700@gmail.com"
-      ]
-    },
-    {
-      "username": "lmoncada",
-      "password": "123456",
-      "information": [
-          "name": "Luis Moncada",
-          "age": 27
-          "email": "mail@gmail.com"
-      ]
-    }
-]
+{
+	"credentials": [{
+			"username": "jvelazquez",
+			"password": "123456",
+			"information": [{
+				"name": "Eduardo Velazquez",
+				"age": 27,
+				"email": "eduardovelazquez700@gmail.com"
+			}]
+		},
+		{
+			"username": "lmoncada",
+			"password": "123456",
+			"information": [{
+				"name": "Luis Moncada",
+				"age": 27,
+				"email": "mail@gmail.com"
+			}]
+		}
+	]
+}
